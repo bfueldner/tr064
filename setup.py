ced1541bd46f652d67c47e55b472736b52a56f81
Benjamin Füldner <benjamin@fueldner.net>
2020-10-12 16:45:49 +0200

    Split up library and add test server.

--- setup.py
+++ setup.py
@@ -1,21 +1,40 @@
+"""TR-064 setuptools configuration."""
+
 import setuptools
+import tr064.version
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="tr064",
-    version="0.0.1",
-    author="Benjamin Füldner",
-    author_email="benjamin@fueldner.net",
+    version=tr064.__version__,
     description="TR-064 python API",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    keywords="TR-064 dslforum router AVM Fritz!Box",
+    license="MIT",
+    author="Benjamin Füldner",
+    author_email="benjamin@fueldner.net",
     url="https://code.fueldner.net/opensource/tr064",
+    project_urls={
+        'Source': 'https://github.com/bfueldner/tr064',
+        'Bug Reports': 'https://github.com/bfueldner/tr064/issues',
+    },
     packages=setuptools.find_packages(),
     classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
+    python_requires='>=3.5',
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"]
 )
---
+++ tr064/config.py
@@ -0,0 +1,14 @@
+"""TR-064 configuration."""
+
+__all__ = [
+    'TR064_DEVICE_NAMESPACE',
+    'TR064_SERVICE_NAMESPACE',
+]
+
+TR064_DEVICE_NAMESPACE = {
+    '': 'urn:dslforum-org:device-1-0'
+}
+
+TR064_SERVICE_NAMESPACE = {
+    '': 'urn:dslforum-org:service-1-0'
+}
