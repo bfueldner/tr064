88d21da29c74ecf0976744c38ff86b0f1bcf95af
Benjamin Füldner <benjamin@fueldner.net>
2020-10-13 12:22:17 +0200

    Deduce super() call.

--- tr064/service_list.py
+++ tr064/service_list.py
@@ -12,6 +12,6 @@ class ServiceList(list):
     def __getitem__(self, index):
         """Overriden braket operator to return TR-064 exception."""
         if len(self) > index:
-            return super(ServiceList, self).__getitem__(index)
+            return super().__getitem__(index)
 
         raise TR064UnknownServiceIndexException
